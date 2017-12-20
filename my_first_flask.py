de521c2bd1a9246531af96d07661baa5b28df960
Gena Catherine <gena.catharine@gmail.com>
2017-12-20 12:24:39 -0700

    renders template

--- my_first_flask.py
+++ my_first_flask.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello world hola mundo'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User {}'.format(username)
+
+from flask import render_template
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
