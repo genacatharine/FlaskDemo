59b2dd5cbf218e406cf4cafe5e9ecb31acf986cd
Gena Catherine <gena.catharine@gmail.com>
2017-12-26 11:40:53 -0700

    attempt to integrate with react views

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from flask import Flask
-from flask import render_template
-app = Flask(__name__)
+from flask import render_template, redirect, flash, request, session, abort
+app = Flask(__name__, template_folder="../capstone_react/src/components/")
 # console.log(Flask(__name__))
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world hola mundo'
+
 @app.route('/')
-def hello_world():
-    return 'Hello world hola mundo'
+def home():
+    if not session.get('logged_in'):
+        return render_template('Login.js')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username']=='admin':
+        session['logged_in']= True
+    else:
+        flash('wrong password!')
+        return home()
 
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User {}'.format(username)
-
-from flask import render_template
+# from flask import render_template
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
